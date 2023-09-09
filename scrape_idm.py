9029c6cbb4a8232b1ade0141e711908278c0d886
Wahyu Dwi Prasetio <wdprsto@gmail.com>
2023-09-09 20:37:20 +0700

    berhasil scrape list data

--- scrape_idm.py
+++ scrape_idm.py
@@ -31,6 +31,7 @@ LIST SEMUA SUBKATEGORI
 """
 # init variabel
 sub_cat_list = {}
+CSV_FILE_NAME = "test.csv"
 
 # init subcategory
 html = driver.page_source
@@ -43,15 +44,48 @@ for sub_cat in beautifulSoupText.select('div[class*="section-kategori"]'):
     # except: 
     #     continue
 
-print(sub_cat_list)
+# print(sub_cat_list)
+"""
+INITIALISASI CSV PENAMPUNG
+"""
+with open(CSV_FILE_NAME, "a", newline='') as csv_file:
+    writer = csv.writer(csv_file)
+    writer.writerow(['prod_name','prod_cat','prod_link'])
 
 """
 ITERASI UNTUK TIAP SUB KATEGOIR
 """
 
 for sc_name, sc_link in sub_cat_list.items():
+    data = []
+
     # buka linknya
     driver.get(sc_link)
+    sub_html = driver.page_source
 
+    sub_beautifulSoupText = BeautifulSoup(sub_html, "html.parser")
 
-    break; 
+    # definisikan variabel penting
+    MAX_PAGE = sub_beautifulSoupText.select_one('select[class*="pagelist"] option:last-child').text # max page of the subcategory..
+    # selector can be modified to get specific element inside it. co: div:nth-of-type(2)
+    
+    for idx in range(1, int(MAX_PAGE)+1):
+        if not idx == 1: 
+            driver.get(f"{sc_link}?page={idx}")
+            sub_html = driver.page_source
+
+            sub_beautifulSoupText = BeautifulSoup(sub_html, "html.parser")
+
+        items = sub_beautifulSoupText.select('div[class*="product-collection"] > div[class="item"]')
+        
+        for item in items:
+            prod_link = item.select_one('a').get('href')
+            prod_name = item.find('div', attrs={'class':'title'}).text.strip()
+            data.append((prod_name, sc_name, prod_link))
+
+    # write to csv
+    with open(CSV_FILE_NAME, "a", newline='', encoding="utf-8") as csv_file:
+        writer = csv.writer(csv_file)
+        # The for loop
+        for prod_name, prod_cat, prod_link in data:
+            writer.writerow([prod_name, prod_cat, prod_link])
